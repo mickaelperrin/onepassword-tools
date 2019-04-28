0360d13f5cb3b253f0ed2db49f78eba1402ba34a
Mickaël Perrin <dev@mickaelperrin.fr>
2019-04-28 10:55:41 +0200

    Import initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# 1Password Tools
+
+> This is a collection of 1Password related CLI tools and helpers.
---
+++ onepassword_tools/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+
+def start():
+    print('Start')
+
+
+if __name__ == '__main__':
+    start()
