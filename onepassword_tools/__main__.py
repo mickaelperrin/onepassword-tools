917255997a8e37689ef25a6b8ffb052748249901
Mickaël Perrin <dev@mickaelperrin.fr>
2019-05-08 19:02:04 +0200

    [FEATURE] new-database-account

--- onepassword_tools/__main__.py
+++ onepassword_tools/__main__.py
@@ -5,6 +5,7 @@ from onepassword_tools.ssh_add import ssh_add
 from onepassword_tools.new_server_account import new_server_account
 from onepassword_tools.new_ssh_key import new_ssh_key
 from onepassword_tools.new_website_account import new_website_account
+from onepassword_tools.new_database_account import new_database_account
 from onepassword_tools.lib.Log import logger
 click_log.basic_config(logger)
 
@@ -19,6 +20,7 @@ start.add_command(ssh_add.ssh_add)
 start.add_command(new_server_account.new_server_account)
 start.add_command(new_ssh_key.new_ssh_key)
 start.add_command(new_website_account.new_website_account)
+start.add_command(new_database_account.new_database_account)
 
 if __name__ == '__main__':
     start()
---
+++ onepassword_tools/lib/OnePasswordDatabaseItem.py
@@ -0,0 +1,99 @@
+from onepassword_tools.lib.OnePasswordItem import OnePasswordItem
+
+
+class OnePasswordDatabaseItem(OnePasswordItem):
+
+    database: str = None
+    hostname: str = None
+    item_type: str = 'Database'
+    notes: str = None
+    password: str = None
+    port: str = None
+    url: str = None
+    username: str = None
+
+    def __getitem__(self, item):
+        return super().__getitem__(item)
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                self.__setattr__(key, value)
+
+    def _request_object(self):
+        """
+        Return the dict object with the request sent to 1Password
+        :return: dict
+        """
+        return {
+            "notesPlain": self.get('notes'),
+            "sections": [
+                {
+                    "title": "",
+                    "name": "Section_%s" % self.opu.generate_op_section_uuid(),
+                    "fields": [
+                        {
+                            "k": "menu",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "t": "type",
+                        },
+                        {
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "v": self.hostname,
+                            "t": "server"
+                        },
+                        {
+                            "inputTraits": {
+                                "keyboard": "NumberPad"
+                            },
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "v": self.port,
+                            "t": "port"
+                        },
+                        {
+                            "inputTraits": {
+                                "autocapitalization": "none",
+                                "autocorrection": "no"
+                            },
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "v": self.database,
+                            "t": "database"
+                        },
+                        {
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "v": self.username,
+                            "t": "username"
+                        },
+                        {
+                            "k": "concealed",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "v": self.password,
+                            "t": "password"
+                        },
+                        {
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "t": "SID"
+                        },
+                        {
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "t": "alias"
+                        },
+                        {
+                            "k": "string",
+                            "n": self.opu.generate_op_field_uuid(),
+                            "t": "connection options"
+                        }
+                    ]
+                }
+            ]
+        }
+
+    def get_title(self):
+        return "USER %s FOR DATABASE %s ON %s" % (self.username, self.database, self.hostname)
